26210a74720ef5520753371145e0063049f00061
lufefe <jumbaayabonga@gmail.com>
2023-01-31 21:57:58 -0500

    Update models.py

--- news/models.py
+++ news/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Feed(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=500)
     url = models.URLField()
     is_active = models.BooleanField(default=False)
 
@@ -12,12 +12,12 @@ class Feed(models.Model):
 
 class Article(models.Model):
     feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=500)
     url = models.URLField()
     media = models.URLField(default=None)
     description = models.TextField()
     publication_date = models.DateTimeField()
-    author = models.CharField(max_length=200)
+    author = models.CharField(max_length=500)
 
     def __str__(self):
         return self.title
