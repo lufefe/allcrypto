1eaae485574b9f0fbc81fc3ec9dea26eb9f687a5
lufefe <jumbaayabonga@gmail.com>
2023-02-01 01:26:25 -0500

    Update views.py

--- news/views.py
+++ news/views.py
@@ -8,25 +8,22 @@ from news.models import Article, Feed
 # todo : enhance logic by using one object to store multiple feeds; use random numbers to assign feeds from database.
 def articles_list(request): #todo update feeds and numbers in objects.filter statement
     # Source 1 - https://www.coindesk.com/arc/outboundfeeds/rss/
-    articles1 = Article.objects.filter(feed = 11).order_by('-publication_date')
+    articles1 = Article.objects.filter(feed = 21).order_by('-publication_date')
     rows1 = [articles1[x:x+1] for x in range(0, len(articles1), 1)]
 
     # Source 2 - https://cointelegraph.com/rss
-    articles2 = Article.objects.filter(feed = 12).order_by('-publication_date')
+    articles2 = Article.objects.filter(feed = 22).order_by('-publication_date')
     rows2 = [articles2[x:x+1] for x in range(0, len(articles2), 1)]
 
     # Source 3 - https://crypto.news/feed/
-    articles3 = Article.objects.filter(feed = 13).order_by('-publication_date')
+    articles3 = Article.objects.filter(feed = 23).order_by('-publication_date')
     rows3 = [articles3[x:x+1] for x in range(0, len(articles3), 1)]
 
     # Source 4 - https://coinstats.app/blog/feed/
-    articles4 = Article.objects.filter(feed = 19).order_by('-publication_date')
+    articles4 = Article.objects.filter(feed = 24).order_by('-publication_date')
     rows4 = [articles4[x:x+1] for x in range(0, len(articles4), 1)]
 
     # Source 5 - Podcast
-    articles5 = Article.objects.filter(feed = 15).order_by('-publication_date')
-    rows5 = [articles5[x:x+1] for x in range(0, len(articles5), 1)]
-
     
     return render(request, 'articles_list.html', {'rows1': rows1, 'rows2': rows2, 'rows3': rows3, 'rows4': rows4, 'rows5': rows5})
 
