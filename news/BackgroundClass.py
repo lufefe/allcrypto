3af6c0d01e1bf66a327783f12a887f6d852c69ad
lufefe <jumbaayabonga@gmail.com>
2023-02-19 00:09:24 -0500

    Update BackgroundClass.py

--- news/BackgroundClass.py
+++ news/BackgroundClass.py
@@ -23,7 +23,7 @@ class BackgroundClass:
                     if ent.title == art.title:
                         already_updated = True
                     else:
-                        get_feed_id = Feed.objects.get(url = artitle.title_detail.base)
+                        get_feed_id = Feed.objects.get(url = article.title_detail.base)
 
                         article = Article()
                         article.title = artitle.title
